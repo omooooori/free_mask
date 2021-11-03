f1163cb8994c2b328ce0d4dbc939ed7309ce3ad9
omooooori <r.shimizu18020@gmail.com>
2021-11-03 17:47:17 +0900

    Implement CameraPreview and resolve #1.

--- app/src/main/java/com/omooooori/free_mask/MainActivity.kt
+++ app/src/main/java/com/omooooori/free_mask/MainActivity.kt
@@ -1,43 +1,68 @@
 package com.omooooori.free_mask
 
+import android.Manifest
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.provider.Settings
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.unit.dp
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.omooooori.free_mask.domain.util.Permission
+import com.omooooori.free_mask.presentation.camera.CameraPreview
 import com.omooooori.free_mask.ui.theme.Free_maskTheme
 
+@ExperimentalPermissionsApi
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             Free_maskTheme {
-                // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainContent(Modifier.fillMaxSize())
                 }
             }
         }
     }
 }
 
+@ExperimentalPermissionsApi
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    Free_maskTheme {
-        Greeting("Android")
+fun MainContent(modifier: Modifier = Modifier) {
+    val context = LocalContext.current
+    Permission(
+        permission = Manifest.permission.CAMERA,
+        rationale = "You said you wanted a picture, so I'm going to have to ask for permission.",
+        permissionNotAvailableContent = {
+            Column(modifier) {
+                Text("O noes! No Camera!")
+                Spacer(modifier = Modifier.height(8.dp))
+                Button(
+                    onClick = {
+                        context.startActivity(
+                            Intent(Settings.ACTION_APPLICATION_DETAILS_SETTINGS).apply {
+                                data = Uri.fromParts("package", context.packageName, null)
+                            }
+                        )
+                    }
+                ) {
+                    Text("Open Settings")
+                }
+            }
+        }
+    ) {
+        CameraPreview(modifier)
     }
 }
